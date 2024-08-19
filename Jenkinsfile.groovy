b18704cdb7c359b581cafffb5a6ab85b7606bf63
MeranFarahat <Meranfarahat@gmail.com>
2024-08-19 22:05:05 +0300

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,37 +1,41 @@
-`pipeline {
+pipeline {
     agent any
     
     stages {
-
-        stage{'Get code from github'} {
-
+        stage('Get code from GitHub') {
             steps {
-                git branch 'main',
-                credentialsId: '0fb76644-59be-470a-a3dc-b73810d0c458',
-                url: 'https://github.com/moustafa-ismail/sprints-project'
+                git branch: 'main',
+                    credentialsId: '0fb76644-59be-470a-a3dc-b73810d0c458',
+                    url: 'https://github.com/moustafa-ismail/sprints-project'
                 sh 'cd Sprints_W5-_Final'
                 sh 'ls -altr'
             }
         }
         stage('Build Docker Image') {
             steps {
-                sh 'docker build -t mohsen0808/project:latest .'
+                sh 'docker build -t flaskapp:latest .'
             }
         }
         stage('Push Image to Nexus') {
             steps {
                 script {
-                    // Replace with your Nexus credentials
-                    nexusUser = 'admin'
-                    nexusPassword = '123'
+                    def nexusUser = 'admin'
+                    def nexusPassword = '123'
+                    def nexusUrl = 'http://ad88db502381e46a4b47f813036bb426-421067397.us-west-1.elb.amazonaws.com:8085/repository/sprints-project/'
+                    def nexusHostname = 'ad88db502381e46a4b47f813036bb426-421067397.us-west-1.elb.amazonaws.com:8085'
+                    def imageName = 'flaskapp:latest'
 
-                    // Replace with your Nexus repository URL and image name
-                    nexusUrl = 'http://ad88db502381e46a4b47f813036bb426-421067397.us-west-1.elb.amazonaws.com:8081/'
-                    imageName = 'mohsen0808/project:latest'
+                    // Log in to Nexus
+                    sh "echo ${nexusPassword} | docker login -u ${nexusUser} -p ${nexusPassword} ${nexusUrl}"
 
-                    //sh "docker login -u ${nexusUser} -p ${nexusPassword} ${nexusUrl}"
-                    sh "docker push ${nexusUrl}/${imageName}"
-                    //sh "docker logout ${nexusUrl}"
+                    // Tag the image with the Nexus repository URL
+                    sh "docker tag flaskapp:latest ${nexusHostname}/flaskapp:latest"
+
+                    // Push the image to Nexus
+                    sh "docker push ${nexusHostname}/flaskapp:latest"
+
+                    // Log out from Nexus
+                    sh "docker logout ${nexusHostname}"
                 }
             }
         }
